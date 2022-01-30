b4cf3f4f9507dd83a1b18b50395a94823adf216e
hrozllzl <hrotlzl@gmail.com>
2022-01-30 19:28:18 +0900

    MainActivity.kt 작성
    키와 몸무게 입력 기능 구현
    결과 화면 연결

---
+++ app/src/main/java/fastcampus/aop/part2/chaper01/ResultActivity.kt
@@ -0,0 +1,12 @@
+package fastcampus.aop.part2.chaper01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
